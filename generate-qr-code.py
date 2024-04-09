61f206d289b98ff3ba90b3d113abd3ff6b1d0ad3
luhem7 <mehul.gang@gmail.com>
2024-04-09 08:38:55 -0400

    Refactored code

--- generate-qr-code.py
+++ generate-qr-code.py
@@ -2,24 +2,25 @@ import io
 from PIL import Image
 import segno
 
-if __name__ == "__main__" :
 
-    qr_code_text = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    embedded_logo_path = 'data/test-cat.png'
-    output_qr_code = 'data/qr_code.png'
-
+def simple_embed_center(qr_code_text, embedded_logo_path, output_qr_code):
     out = io.BytesIO()
-    # Nothing special here, let Segno generate the QR code and save it as PNG in a buffer
     segno.make(qr_code_text, error='h').save(out, scale=5, kind='png')
-    out.seek(0)  # Important to let Pillow load the PNG
+    out.seek(0)
+
     img = Image.open(out)
-    img = img.convert('RGB')  # Ensure colors for the output
+    img = img.convert('RGB')
     img_width, img_height = img.size
-    logo_max_size = img_height // 3  # May use a fixed value as well
-    logo_img = Image.open(embedded_logo_path)  # The logo
-    # Resize the logo to logo_max_size
+    logo_max_size = img_height // 3
+    logo_img = Image.open(embedded_logo_path)
     logo_img.thumbnail((logo_max_size, logo_max_size), Image.Resampling.LANCZOS)
-    # Calculate the center of the QR code
     box = ((img_width - logo_img.size[0]) // 2, (img_height - logo_img.size[1]) // 2)
     img.paste(logo_img, box)
     img.save(output_qr_code)
+
+
+if __name__ == "__main__" :
+    qr_code_text = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    embedded_logo_path = 'data/test-cat.png'
+    output_qr_code = 'data/qr_code.png'
+    simple_embed_center(qr_code_text, embedded_logo_path, output_qr_code)
